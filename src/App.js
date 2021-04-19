a412c2b55cf47e7fc38100c1dfabaf07bf166cb0
AurelBouchet <aurelien-bouchet@hotmail.fr>
2021-04-19 19:55:05 +0200

    simpsqons quest API 2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NewQuote from './components/NewQuote';
+import { useState } from 'react';
+import axios from 'axios';
+
+const sampleQuote = [
+  {
+    "quote": "I can't even say the word 'titmouse' without gigggling like a schoolgirl.",
+    "character": "Homer Simpson",
+    "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939",
+  }
+];
 
 function App() {
+  
+  const [quote, setQuote] = useState(sampleQuote);
+  
+  const getQuote= () => {
+    // Send the request
+    axios
+      .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+      // Extract the DATA from the received response
+      .then((response) => response.data)
+      // Use this data to update the state
+      .then((data) => {
+        setQuote(data);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <NewQuote quote={quote[0]} />
+     <button type="button" onClick={getQuote}>Get a new quote !</button>
     </div>
   );
 }
---
+++ src/components/NewQuote.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function NewQuote({ quote }) {
+  return (
+    quote && (
+      <div className='NewQuote'>
+        <h1>{quote.character}</h1>
+		        <h3>"{quote.quote}"</h3>
+            <br />
+			      <img src={quote.image} alt="simpson character" height="400"/>
+      </div>
+    )
+  );
+}
+
+export default NewQuote;
